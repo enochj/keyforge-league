9114e4832380e9ffa98a525c4c0156c100e1088e
Enoch Jennison <enochj@hightouchinc.com>
2018-12-20 16:31:09 -0600

    curl test in leagues route

--- app/Http/Controllers/LeagueController.php
+++ app/Http/Controllers/LeagueController.php
@@ -14,7 +14,19 @@ class LeagueController extends Controller
      */
     public function index()
     {
-        //
+        // Get cURL resource
+        $curl = curl_init();
+        // Set some options - we are passing in a useragent too here
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => 'https://www.keyforgegame.com/api/decks/?page=0&page_size=10&links=cards',
+        ));
+        // Send the request & save response to $resp
+        $resp = curl_exec($curl);
+        // Close request to clear up some resources
+        curl_close($curl);
+
+        dd($resp);
     }
 
     /**
